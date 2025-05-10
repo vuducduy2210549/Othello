cad34c0c73f0799fc46e0a9aaad220eec6a03021
ngochoaphan2004 <phanngochoa2004@gmail.com>
2025-05-10 16:05:38 +0700

    remove comment

--- engines/alpha.py
+++ engines/alpha.py
@@ -18,8 +18,6 @@ class AlphaEngine(Engine):
     node_list = []
     branch_list = [0,0,0]
     
-    """ Game engine that implements a simple fitness function maximizing the
-    difference in number of pieces in the given color's favor. """
     def __init__(self):
         self.alpha_beta = False
         self.ply_maxmin = 4
@@ -28,8 +26,6 @@ class AlphaEngine(Engine):
 
     def get_move(self, board, color, move_num=None,
                  time_remaining=None, time_opponent=None):
-        """ Return a move for the given color that maximizes the difference in 
-        number of pieces for that color. """
         # Get a list of all legal moves.
         # moves = board.get_legal_moves(color)
 
@@ -279,8 +275,6 @@ class AlphaEngine(Engine):
         return total
 
     def greedy(self, board, color, move):
-        """ Return the difference in number of pieces after the given move 
-        is executed. """
 
         # Create a deepcopy of the board to preserve the state of the actual board
         newboard = deepcopy(board)
@@ -294,8 +288,6 @@ class AlphaEngine(Engine):
         return num_pieces_me - num_pieces_op
 
     def _get_cost(self, board, color):
-        """ Return the difference in number of pieces after the given move 
-        is executed. """
 
         # Create a deepcopy of the board to preserve the state of the actual board
         #newboard = deepcopy(board)
--- engines/greedy.py
+++ engines/greedy.py
@@ -2,12 +2,8 @@ from engines import Engine
 from copy import deepcopy
 
 class GreedyEngine(Engine):
-    """ Game engine that implements a simple fitness function maximizing the
-    difference in number of pieces in the given color's favor. """
     def get_move(self, board, color, move_num=None,
                  time_remaining=None, time_opponent=None):
-        """ Return a move for the given color that maximizes the difference in 
-        number of pieces for that color. """
         # Get a list of all legal moves.
         moves = board.get_legal_moves(color)
 
@@ -17,8 +13,6 @@ class GreedyEngine(Engine):
         return max(moves, key=lambda move: self._get_cost(board, color, move))
 
     def _get_cost(self, board, color, move):
-        """ Return the difference in number of pieces after the given move 
-        is executed. """
 
         # Create a deepcopy of the board to preserve the state of the actual board
         newboard = deepcopy(board)
